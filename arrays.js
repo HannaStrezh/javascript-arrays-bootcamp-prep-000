83cd915813a37e493995757a110a45e216ee2e9d
Hanna Strezh <hannastrezh@gmail.com>
2018-07-23 23:05:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,6 +19,6 @@ function accessElementInArray(array,index){
   return array [index];
 }
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  return array.slice([0]);
+  return array.slice(1);
   
 }
